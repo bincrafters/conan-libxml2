a7f34c00ff76373e36cc35f8c33f8f6425c1a8ad
Luis Martinez <lasote@gmail.com>
2015-12-28 11:45:37 +0100

    windows almost working

--- build.py
+++ build.py
@@ -1,33 +1,80 @@
 import os
 import platform
 import sys
+from subprocess import call, Popen, PIPE
+import commands
 
 if __name__ == "__main__":
     os.system('conan export lasote/stable')
    
-    def test(settings):
+    def test(settings, visual_version=None):
         argv =  " ".join(sys.argv[1:])
-        command = "conan test %s %s" % (settings, argv)
+        curdir = os.path.abspath(os.path.curdir)
+        if visual_version:
+            vcvars = 'call "%vs' +  str(visual_version) + '0comntools%../../VC/vcvarsall.bat"'
+            param = "x86" if "arch=x86 " in settings else "amd64"
+            command = '%s %s && conan test . %s %s' % (vcvars, param, settings, argv)
+        else:
+            command = 'conan test . %s %s' % (settings, argv)
         retcode = os.system(command)
         if retcode != 0:
             exit("Error while executing:\n\t %s" % command)
 
-       
-    # Static x86
-    test('-s arch=x86 -s build_type=Debug -o libxml2:shared=False')
-    test('-s arch=x86 -s build_type=Release -o libxml2:shared=False')
-    # Shared x86
-    test('-s arch=x86 -s build_type=Debug -o libxml2:shared=True')
-    test('-s arch=x86 -s build_type=Release -o libxml2:shared=True')
- 
-    
+        
+    if platform.system() == "Windows":
+        for visual_version in [ 12, 10, 14]:
+            compiler = "-s compiler=\"Visual Studio\" -s compiler.version=%s " % str(visual_version)
+            
+            # Shared x86
+            test(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MDd -o libxml2:shared=True', visual_version)
+            test(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MTd -o libxml2:shared=True', visual_version)
+            test(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MD -o libxml2:shared=True', visual_version)
+            test(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MT -o libxml2:shared=True', visual_version)
 
-    # Shared x86_64
-    test('-s arch=x86_64 -s build_type=Debug -o libxml2:shared=True')
-    test('-s arch=x86_64 -s build_type=Release -o libxml2:shared=True')
- 
-    # Static x86_64
-    test('-s arch=x86_64 -s build_type=Debug -o libxml2:shared=False')
-    test('-s arch=x86_64 -s build_type=Release -o libxml2:shared=False')
+            # Static x86
+            test(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MTd -o libxml2:shared=False', visual_version)
+            test(compiler + '-s arch=x86 -s build_type=Debug -s compiler.runtime=MDd -o libxml2:shared=False', visual_version)
+            test(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MD -o libxml2:shared=False', visual_version)
+            test(compiler + '-s arch=x86 -s build_type=Release -s compiler.runtime=MT -o libxml2:shared=False', visual_version)
+
+              
+            
+            if visual_version != 10:
+                
+                # Static x86_64
+                test(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MDd -o libxml2:shared=False', visual_version)
+                test(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MTd -o libxml2:shared=False', visual_version)
+                test(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MD -o libxml2:shared=False', visual_version)
+                test(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MT -o libxml2:shared=False', visual_version)
+               
+       
+                # Shared x86_64
+                test(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MDd -o libxml2:shared=True', visual_version)
+                test(compiler + '-s arch=x86_64 -s build_type=Debug -s compiler.runtime=MTd -o libxml2:shared=True', visual_version)
+                test(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MD -o libxml2:shared=True', visual_version)
+                test(compiler + '-s arch=x86_64 -s build_type=Release -s compiler.runtime=MT -o libxml2:shared=True', visual_version)
+
+
+                   
+           
+     
+    else:
+           
+        # Static x86
+        test('-s arch=x86 -s build_type=Debug -o libxml2:shared=False')
+        test('-s arch=x86 -s build_type=Release -o libxml2:shared=False')
+        # Shared x86
+        test('-s arch=x86 -s build_type=Debug -o libxml2:shared=True')
+        test('-s arch=x86 -s build_type=Release -o libxml2:shared=True')
+     
+        
+    
+        # Shared x86_64
+        test('-s arch=x86_64 -s build_type=Debug -o libxml2:shared=True')
+        test('-s arch=x86_64 -s build_type=Release -o libxml2:shared=True')
+     
+        # Static x86_64
+        test('-s arch=x86_64 -s build_type=Debug -o libxml2:shared=False')
+        test('-s arch=x86_64 -s build_type=Release -o libxml2:shared=False')
  
    
--- conanfile.py
+++ conanfile.py
@@ -14,32 +14,79 @@ class Bzip2Conan(ConanFile):
     options = {"shared": [True, False]}
     default_options = "shared=False"
     exports = ["CMakeLists.txt"]
-    url="http://github.com/lasote/conan-libxml2"
+    url = "http://github.com/lasote/conan-libxml2"
     requires = "zlib/1.2.8@lasote/stable"
 
     def source(self):
         zip_name = "libxml2-%s.tar.gz" % self.version
         download("ftp://xmlsoft.org/libxml2/%s" % zip_name, zip_name)
         check_md5(zip_name, "daece17e045f1c107610e137ab50c179")
-        unzip(zip_name)        
+        unzip(zip_name)
         os.unlink(zip_name)
         
     def config(self):
         self.options["zlib"].shared = self.options.shared
-
-    def generic_env_configure_vars(self):
+        if self.settings.os == "Windows":
+            self.requires.add("winiconv/1.14.0@lasote/stable", private=False)
+            self.options.shared = True # Static in win doesn't work, runtime errors
+            self.options["zlib"].shared = True
+        
+    def generic_env_configure_vars(self, verbose=False):
         """Reusable in any lib with configure!!"""
-        libs = 'LIBS="%s"' % " ".join(["-l%s" % lib for lib in self.deps_cpp_info.libs])
-        ldflags = 'LDFLAGS="%s"' % " ".join(["-L%s" % lib for lib in self.deps_cpp_info.lib_paths]) 
-        archflag = "-m32" if self.settings.arch == "x86" else ""
-        cflags = 'CFLAGS="%s %s"' % (archflag, " ".join(self.deps_cpp_info.cflags))
-        cpp_flags = 'CPPFLAGS="%s %s"' % (archflag, " ".join(self.deps_cpp_info.cppflags))
-        return "%s %s %s %s" % (libs, ldflags, cflags, cpp_flags)
+        if self.settings.os == "Linux" or self.settings.os == "Macos":
+            libs = 'LIBS="%s"' % " ".join(["-l%s" % lib for lib in self.deps_cpp_info.libs])
+            ldflags = 'LDFLAGS="%s"' % " ".join(["-L%s" % lib for lib in self.deps_cpp_info.lib_paths]) 
+            archflag = "-m32" if self.settings.arch == "x86" else ""
+            cflags = 'CFLAGS="%s %s"' % (archflag, " ".join(self.deps_cpp_info.cflags))
+            cpp_flags = 'CPPFLAGS="%s %s"' % (archflag, " ".join(self.deps_cpp_info.cppflags))
+            command = "env %s %s %s %s" % (libs, ldflags, cflags, cpp_flags)
+        elif self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
+            cl_args = " ".join(['/I"%s"' % lib for lib in self.deps_cpp_info.include_paths])
+            lib_paths= ";".join(['"%s"' % lib for lib in self.deps_cpp_info.lib_paths])
+            command = "SET LIB=%s;%%LIB%% && SET CL=%s" % (lib_paths, cl_args)
+            if verbose:
+                command += " && SET LINK=/VERBOSE"
+        
+        return command
        
     def build(self):
+        if self.settings.os == "Windows":
+            self.build_windows()
+        else:
+            self.build_with_configure()
+            
         
+    def build_windows(self):
+        iconv_headers_paths = self.deps_cpp_info["winiconv"].include_paths[0]
+        iconv_lib_paths= " ".join(['lib="%s"' % lib for lib in self.deps_cpp_info["winiconv"].lib_paths])
+        
+        env_variables = self.generic_env_configure_vars()
+        compiler = "msvc" if self.settings.compiler == "Visual Studio" else self.settings.compiler == "gcc"
+        debug = "yes" if self.settings.build_type == "Debug" else "no"
+        
+        configure_command = "cd %s/win32 && %s && cscript configure.js " \
+                            "zlib=1 compiler=%s cruntime=/%s debug=%s include=\"%s\" %s" % (self.ZIP_FOLDER_NAME, 
+                                                                               env_variables,
+                                                                               compiler, 
+                                                                               self.settings.compiler.runtime,
+                                                                               debug, 
+                                                                               iconv_headers_paths, 
+                                                                               iconv_lib_paths) 
+        self.output.warn(configure_command)
+        self.run(configure_command)
+        
+        makefile_path = os.path.join(self.ZIP_FOLDER_NAME, "win32", "Makefile.msvc")
+        # Zlib library name is not zlib.lib always, it depends on configuration
+        replace_in_file(makefile_path, "LIBS = $(LIBS) zlib.lib", "LIBS = $(LIBS) %s.lib" % self.deps_cpp_info["zlib"].libs[0])
+        
+        make_command = "nmake /f Makefile.msvc" if self.settings.compiler == "Visual Studio" else "make -f Makefile.mingw"
+        make_command = "cd %s/win32 && %s && %s" % (self.ZIP_FOLDER_NAME, env_variables, make_command)
+        self.output.warn(make_command)
+        self.run(make_command)
+        
+    def build_with_configure(self): 
         zlib = "--with-zlib=%s" % self.deps_cpp_info["zlib"].lib_paths[0]
-        configure_command = "cd %s && env %s ./configure %s --with-python=no" % (self.ZIP_FOLDER_NAME, 
+        configure_command = "cd %s && %s ./configure %s --with-python=no" % (self.ZIP_FOLDER_NAME, 
                                                                                  self.generic_env_configure_vars(), 
                                                                                  zlib)
         self.output.warn(configure_command)
@@ -51,12 +98,15 @@ class Bzip2Conan(ConanFile):
         self.copy("*.h", "include", "%s/include" % (self.ZIP_FOLDER_NAME), keep_path=True)
         if self.options.shared:
             self.copy(pattern="*.so*", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
+            self.copy(pattern="*.dll*", dst="bin", src=self.ZIP_FOLDER_NAME, keep_path=False)
         else:
             self.copy(pattern="*.a", dst="lib", src="%s" % self.ZIP_FOLDER_NAME, keep_path=False)
-            self.copy(pattern="*.lib", dst="lib", src="%s" % self.ZIP_FOLDER_NAME, keep_path=False)
-
+        
+        self.copy(pattern="*.lib", dst="lib", src="%s" % self.ZIP_FOLDER_NAME, keep_path=False)
+        
     def package_info(self):
         if self.settings.os == "Linux" or self.settings.os == "Macos":
             self.cpp_info.libs = ['xml2', 'm']
         else:
-            self.cpp_info.libs = ['libxml2']
+            self.cpp_info.libs = ['libxml2'] 
+   
