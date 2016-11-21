14aa74ecab997a80bfeca807b2fbc17915193905
Luis <lasote@gmail.com>
2016-11-21 12:32:43 +0100

    Recovered x86 builds

--- build.py
+++ build.py
@@ -6,14 +6,6 @@ if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="libxml2:shared", pure_c=True)
 
-    if platform.system() == "Darwin":
-        filtered_builds = []
-        for settings, options in builder.builds:
-            # Some isues with OSx and x86 with iconv, I think it could be linking with the system iconv and fails to link.
-            if settings["arch"] != "x86":
-                filtered_builds.append([settings, options])
-        builder.builds = filtered_builds
-
     if platform.system() == "Windows":
         filtered_builds = []
         for settings, options in builder.builds:
