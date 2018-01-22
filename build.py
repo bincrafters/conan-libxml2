67bf8cf5abae4c3798040458bcf2eb493f091f2a
theirix <theirix@gmail.com>
2018-01-22 13:10:09 +0300

    fixed build.py

--- build.py
+++ build.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
 
-    if platform.system() == "Windows":
-        filtered_builds = []
-        for build in builder.builds:
-            if build.settings["compiler"] != "Visual Studio" or build.options[name + ":shared"]:
-                filtered_builds.append(build)
-        builder.builds = filtered_builds
+    if platform.system() == "Windows" or True:
+        items = []
+        for item in builder.items:
+            if item.settings["compiler"] != "Visual Studio" or item.options[name + ":shared"]:
+                items.append(item)
+        builder.items = items
 
     builder.run()
