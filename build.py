54e36b91a00b73570b579e3b89ad5f11bb9a5102
SSE4 <tomskside@gmail.com>
2019-07-18 21:46:12 +0200

    - simplify build.py
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -8,20 +8,4 @@ from bincrafters import build_template_default
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder(pure_c=True)
-
-    items = []
-    for item in builder.items:
-        # add msys2 and mingw as a build requirement for mingw builds
-        if platform.system() == "Windows" and item.settings["compiler"] == "gcc" and \
-                not item.options.get("libxml2:shared", False):
-            new_build_requires = copy.copy(item.build_requires)
-            new_build_requires["*"] = new_build_requires.get("*", []) + \
-                ["mingw_installer/1.0@conan/stable",
-                 "msys2_installer/latest@bincrafters/stable"]
-            items.append([item.settings, item.options, item.env_vars,
-                          new_build_requires, item.reference])
-        else:
-            items.append(item)
-    builder.items = items
-
     builder.run()
